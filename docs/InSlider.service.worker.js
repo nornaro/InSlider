25615ef6e56b81cbe615e45d84cef2386efa8ee0
stevepine <fuyuhimematsuri@gmail.com>
2025-07-23 22:30:56 +0200

    nocomment

--- docs/InSlider.service.worker.js
+++ docs/InSlider.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1753302093|70259209041';
+const CACHE_VERSION = '1753302412|70578792351';
 /** @type {string} */
 const CACHE_PREFIX = 'InSlider-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
