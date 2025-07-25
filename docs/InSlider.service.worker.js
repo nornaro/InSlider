0f5d02547dc9c07df3aaff32407cb32d59d6f201
stevepine <fuyuhimematsuri@gmail.com>
2025-07-25 19:20:48 +0200

    can't find pck

--- docs/InSlider.service.worker.js
+++ docs/InSlider.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1753463817|30012957';
+const CACHE_VERSION = '1753464034|246491173';
 /** @type {string} */
 const CACHE_PREFIX = 'InSlider-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
